6777f5fc27679ed4cc65e3866a3de095ee624855
youke <2788312242@qq.com>
2022-02-08 21:28:08 +0800

    系统整体布局

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './plugins/ant-design-vue';
+// 清除默认样式
+import '@/assets/css/reset.less';
 
 Vue.config.productionTip = false;
 
